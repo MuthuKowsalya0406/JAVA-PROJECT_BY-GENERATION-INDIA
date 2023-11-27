a123deccc57521c15a84ff436076f3a2d5acf4a1
MuthuKowsalya0406 <149670109+MuthuKowsalya0406@users.noreply.github.com>
2023-11-27 08:20:49 +0530

    Add files via upload

---
+++ myfirstinpu/Myfirstinput.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package myfirstinpu;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+public class Myfirstinput {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+		 System.out.println("Please enter your name ");
+		 String name = scanner.next();
+		 System.out.println("My name is "+ name);
+		// TODO Auto-generated method stub
+
+	}
+
+}
